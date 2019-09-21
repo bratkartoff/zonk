bf0f279f018ad8ce7781f1cbd28a5fb9d491d3e6
Matthias Brehmer <m@brehmer-adf.de>
2019-09-21 14:28:35 +0200

    Add dummy chat server

--- app/src/main/java/com/dhbw/zonk/MainActivity.java
+++ app/src/main/java/com/dhbw/zonk/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+	private Server srv = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		srv = new Server(20043);
+		srv.start();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public void onDestroy() {
+		super.onDestroy();
+		srv.stopListening();
+	}
 }
---
+++ app/src/main/java/com/dhbw/zonk/Server.java
@@ -0,0 +1,150 @@
+package com.dhbw.zonk;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import android.util.*;
+
+class Server extends Thread {
+	private final int port;
+	private ServerSocket srvSck = null;
+	private LinkedList<ClientWorker> clients = new LinkedList<>();
+	private long clientID = 0;
+
+	Server(int port) {
+		this.port = port;
+	}
+
+	public void run() {
+		try {
+			srvSck = new ServerSocket(port);
+			while (true) {
+				log("Listening for client");
+				try {
+					ClientWorker cw = new ClientWorker(this, srvSck.accept(), clientID);
+					clients.add(cw);
+					log("Client connected, id " + clientID);
+					clientID++;
+					cw.start();
+				} catch (IOException e) {
+					break;
+				}
+			}
+		} catch (IOException e) {
+			Log.e("Server", "Error creating ServerSocket: " + e);
+		}
+	}
+
+	void stopListening() {
+		for (ClientWorker client : clients)
+			client.requestStop();
+		try {
+			if (srvSck != null)
+				srvSck.close();
+		} catch (IOException e) {
+			Log.e("Server", "Error closing ServerSocket: " + e);
+		}
+	}
+
+	synchronized void send(String msg, long from) {
+		for (ClientWorker client : clients) {
+			if (client.getID() != from)
+				client.print(from + ": " + msg);
+		}
+	}
+
+	synchronized void stopThread(long id) {
+		log("Requesting thread " + id + " to exit");
+		ClientWorker client;
+		ListIterator<ClientWorker> iter = clients.listIterator();
+		while(iter.hasNext()) {
+			client = iter.next();
+			if (client.getID() == id) {
+				log("Found thread " + id + " in clients list");
+				client.requestStop();
+				iter.remove();
+				break;
+			}
+		}
+	}
+
+	synchronized Boolean isValidThreadID(long id) {
+		return clients.stream().anyMatch(client -> client != null && client.getID() == id);
+	}
+
+	private void log(String msg) { Log.v("Server", msg); }
+}
+
+
+class ClientWorker extends Thread {
+	private Socket sck = null;
+	private Server srv = null;
+	private final long id;
+	private Boolean stop = false;
+	private PrintStream outputWriter = null;
+	private Scanner inputScanner = null;
+	
+	ClientWorker(Server srv, Socket sck, long id) throws IOException {
+		this.srv = srv;
+		this.sck = sck;
+		this.id = id;
+		this.inputScanner = new Scanner(sck.getInputStream());
+		this.outputWriter = new PrintStream(sck.getOutputStream());
+	}
+
+	synchronized void requestStop() {
+		log("Received exit signal");
+		stop = true;
+		// Close socket to cause thread to exit
+		try {
+			sck.close();
+		} catch (IOException e) {
+			log(Log.getStackTraceString(e));
+		}
+	}
+	long getID() { return this.id; }
+	void print(String msg) { outputWriter.println(msg); }
+	private void printerr(String msg) { print("Error: " + msg); }
+
+	public void run() {
+		print("Connected as client " + id);
+		String str;
+		while (inputScanner.hasNextLine())
+		{
+			str = inputScanner.nextLine();
+
+			// check for /kick
+			// this is only for testing and should not be implemented here, if at all
+			String[] strlst = str.split(" ");
+			if (strlst[0].equals("/kick")) {
+				if (strlst.length != 2) {
+					printerr("Incorrect number of arguments for /kill threadID");
+				} else {
+					Long threadID = null;
+					try {
+						threadID = Long.valueOf(strlst[1]);
+						if (!srv.isValidThreadID(threadID))	{
+							printerr("\"" + threadID + "\" is not a valid ID");
+						} else {
+							log("Requesting thread " + threadID + " to exit");
+							srv.stopThread(threadID);
+						}
+					} catch (NumberFormatException e) {
+						printerr("Could not convert \"" + strlst[1] + "\" to type \"long\"");
+					}
+				}
+				continue;
+			}
+
+			if (stopRequested())
+				break;
+			srv.send(str, id);
+		}
+		inputScanner.close();
+		outputWriter.flush();
+		log("Exiting");
+	}
+
+	private void log(String msg) { Log.v("Thread " + id, msg); }
+	private synchronized Boolean stopRequested() { return stop; }
+}
