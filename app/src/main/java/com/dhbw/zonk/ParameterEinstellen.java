56bf7582219cad195a20203a5c6714f7896ba759
Matthias Brehmer <m@brehmer-adf.de>
2019-11-05 12:13:04 +0100

    Start lobby from start button

--- app/src/main/java/com/dhbw/zonk/ParameterEinstellen.java
+++ app/src/main/java/com/dhbw/zonk/ParameterEinstellen.java
@@ -2,7 +2,9 @@ package com.dhbw.zonk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ParameterEinstellen extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ParameterEinstellen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parameter_einstellen);
+    }
+
+    public void onClickButtonStart(View v) {
+        // todo: check parameters and pass to gameState
+        // start lobby
+        Intent intent = new Intent(this, Lobby.class);
+        startActivity(intent);
     }
 }
