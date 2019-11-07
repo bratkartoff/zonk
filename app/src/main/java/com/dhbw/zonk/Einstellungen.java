604add4c5a63c6340340f6c6b9221200c7d2a9c1
sassas98 <55536162+sassas98@users.noreply.github.com>
2019-11-07 11:03:52 +0100

    Add music

--- app/src/main/java/com/dhbw/zonk/Einstellungen.java
+++ app/src/main/java/com/dhbw/zonk/Einstellungen.java
@@ -2,7 +2,12 @@ package com.dhbw.zonk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
 
 public class Einstellungen extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class Einstellungen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_einstellungen);
+    }
+
+
+
+    public void onToggleClicked(View view) {
+        ToggleButton toggleButton = view.findViewById(R.id.toggleButton);
+        MediaPlayer mediaPlayer = MainActivity.getMediaPlayer();
+
+        if(((ToggleButton) view).isChecked()) {
+            mediaPlayer.seekTo(0);
+            mediaPlayer.start();
+        } else {
+            mediaPlayer.pause();
+        }
     }
 }
